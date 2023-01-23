6c7bb8bff108c07eb216826aa59cd1232871e581
Lars Schürmann <laaaaar5@gmail.com>
2023-01-23 10:53:25 +0100

    added filter api

--- filter-api-data.js
+++ filter-api-data.js
@@ -1,3 +1,15 @@
 function filterApiData(apiData, mandatoryKeys) {
-  // Add you solution here
+  // apiData=[{},{},..]
+  //mandatoryKeys=[key1,key2,...]
+  //return Array with the Objects that passed the test
+
+  let result = apiData.filter((entry) => {
+    //Object.keys(entry) //[key1,key2,..]
+
+    return mandatoryKeys.every((currentKey) =>
+      Object.keys(entry).includes(currentKey)
+    );
+  });
+
+  return result;
 }
